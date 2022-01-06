9bffb8e7184eb54c5860b6bf120c42d45401834f
William <w.goudie@outlook.com>
2022-01-06 20:28:55 +0000

    second

--- Java/semesterOneCourseWork/Seminar's/src/Seminar4/Pset4.java
+++ Java/semesterOneCourseWork/Seminar's/src/Seminar4/Pset4.java
@@ -5,28 +5,23 @@ import java.util.Scanner;
 public class Pset4 {
 
 	public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	
-	System.out.println("Enter any Digit");
-	
-	
-	
-	while(!sc.hasNextInt()) {
-		
-		System.out.println("This is 5");
-		sc.next();
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter any Digit");
+
+		while (!sc.hasNextInt()) {
+
+			System.out.println("This is 5");
+			sc.next();
+		}
+		int a = sc.nextInt();
+
+		if (a < 0) {
+			System.out.println("This is a neg");
+		} else {
+			System.out.println("This is positive");
+		}
+
 	}
-	int a = sc.nextInt();
-	
-	if (a < 0){
-		System.out.println("This is a neg");
-	}
-	else {
-		System.out.println("This is positive");
-	}
-	
-	
-	}
-	
 
 }
