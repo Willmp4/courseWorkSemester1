da66b4f2f6b2c0e44848cf7ce01a1c9d241daa1d
William <w.goudie@outlook.com>
2022-01-06 18:34:25 +0000

    Second

--- Java/semesterOneCourseWork/Seminar's/src/Seminar9/avrg100Nums.java
+++ Java/semesterOneCourseWork/Seminar's/src/Seminar9/avrg100Nums.java
@@ -6,112 +6,97 @@ import java.util.*;
 
 public class avrg100Nums {
 	static int count;
-	static ArrayList<Integer> big_list = new ArrayList<Integer>(); 
-	static ArrayList<Double>avrgs = new ArrayList<Double>(); 
+	static ArrayList<Integer> big_list = new ArrayList<Integer>();
+	static ArrayList<Double> avrgs = new ArrayList<Double>();
 	static Scanner input = new Scanner(System.in);
 
-	static double total = 0 ;
-	
-	static void total(ArrayList<Integer> nums)
-	{
-		
-		for(double numbers : nums)
-		{
+	static double total = 0;
+
+	static void total(ArrayList<Integer> nums) {
+
+		for (double numbers : nums) {
 			total += numbers;
 		}
-		
-		
+
 	}
-	static double avrg(ArrayList<Integer> nums)
-	{
-		double avg = 0; 
-		avg = total /nums.size();
-		
+
+	static double avrg(ArrayList<Integer> nums) {
+		double avg = 0;
+		avg = total / nums.size();
+
 		return avg;
-		//returns the avrg of the numbers within the list
+		// returns the avrg of the numbers within the list
 	}
-	static void run() 
-	{
-		
+
+	static void run() {
+
 		double max_total = 0;
 		double max_avrg = 0;
-		double temp_total= 0;
+		double temp_total = 0;
 		double temp_avrg = 0;
 		double lowest_total = 10000;
 		double lowest_avrg = 10000;
 		double ltemp_total;
-		double ltemp_avrg ;
+		double ltemp_avrg;
 		ArrayList<Integer> prev_list = null;
 		ArrayList<Integer> lprev_list = null;
-		
-		for(int i=1;i<=10000000;i++) 
-		{
-			ArrayList<Integer>nums = new ArrayList<Integer>(); 
-			//Adds a 100 random numbers to a list		
+
+		for (int i = 1; i <= 1000; i++) {
+			ArrayList<Integer> nums = new ArrayList<Integer>();
+			// Adds a 100 random numbers to a list
 			Random rand = new Random();
 			rand.setSeed(System.nanoTime());
-			while((nums.size() != 10))
-			{
-			    Integer r = rand.nextInt(100);
-			    nums.add(r);
+			while ((nums.size() != 10)) {
+				Integer r = rand.nextInt(100);
+				nums.add(r);
 			}
-			
-			
+
 			total(nums);
 			temp_total = total;
 			temp_avrg = avrg(nums);
-			
+
 			ltemp_total = total;
 			ltemp_avrg = avrg(nums);
-			System.out.println(temp_total);
-			System.out.println(temp_avrg);
-			
-			if(ltemp_avrg < lowest_avrg)
-			{
+
+			if (ltemp_avrg < lowest_avrg) {
 				lowest_avrg = ltemp_avrg;
-				if(ltemp_total <lowest_total)
-				{
+				if (ltemp_total < lowest_total) {
 					lprev_list = nums;
-					
+
 				}
 			}
-			if(ltemp_total < lowest_total)
-			{
+			if (ltemp_total < lowest_total) {
 				lowest_total = ltemp_total;
-				
+
 			}
-			
-			if(temp_avrg > max_avrg)
-			{
+
+			if (temp_avrg > max_avrg) {
 				max_avrg = temp_avrg;
-				if(temp_total > max_total)
-				{
+				if (temp_total > max_total) {
 					prev_list = nums;
-					
+
 				}
 			}
-			if(temp_total > max_total)
-			{
+			if (temp_total > max_total) {
 				max_total = temp_total;
-				
+
 			}
-			System.out.println(nums);
+
 			total = 0;
-			
+
 		}
 		System.out.println();
 		System.out.println(prev_list);
-		System.out.println("The max total is "+ max_total );
-		System.out.println("The max average is "+max_avrg);
+		System.out.println("The max total is " + max_total);
+		System.out.println("The max average is " + max_avrg);
 		System.out.println(lprev_list);
-		System.out.println("The lowest total is "+ lowest_total );
-		System.out.println("The lowest average is "+lowest_avrg);
+		System.out.println("The lowest total is " + lowest_total);
+		System.out.println("The lowest average is " + lowest_avrg);
 	}
-	public static void main(String[] args)
-	{
-	
+
+	public static void main(String[] args) {
+
 		run();
-		
+
 	}
 }
-
