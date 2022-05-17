51082dffb96c42fd4ab67e3e65f62e3ec94e8574
William <w.goudie@outlook.com>
2022-05-17 15:06:14 +0100

    Re-organise

---
+++ programming/Java/Seminar's/src/Seminar9/test.java
@@ -0,0 +1,10 @@
+package Seminar9;
+
+public class test {
+
+  public static void main(String[] args) {
+    int[] myList = { 1, 2, 3, 4 };
+    int number = (int) Math.random() * 101;
+    System.out.println(number);
+  }
+}
